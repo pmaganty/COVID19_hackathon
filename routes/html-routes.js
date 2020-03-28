b20d76ed29c046ad94fcb5c7f194931462a2b80a
pmaganty <pranithamaganty@gmail.com>
2020-03-27 19:26:25 -0500

    adding html routes

---
+++ routes/html-routes.js
@@ -0,0 +1,14 @@
+// Requiring path to so we can use relative routes to our HTML files
+var path = require("path");
+
+//creating routes for sending html data to browser
+module.exports = function (app) {
+
+  app.get("/lostPet", function (req, res) {
+
+  });
+
+  app.get("/foundPets", function (req, res) {
+
+  });
+};
--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 // Requiring necessary npm packages
 var express = require("express");
 var session = require("express-session");
-// Requiring passport as we've configured it
-//var passport = require("./config/passport");
 
 // Setting up port and requiring models for syncing
 var PORT = process.env.PORT || 3000;
 var db = require("./models");
 
-// Creating express app and configuring middleware needed for authentication
+// Creating express app
 var app = express();
 app.use(
   express.urlencoded({
@@ -17,9 +15,6 @@ app.use(
 );
 app.use(express.json());
 app.use(express.static("public"));
-
-//app.use(passport.initialize());
-//app.use(passport.session());
 
 // Requiring our routes
 //require("./routes/html-routes.js")(app);
